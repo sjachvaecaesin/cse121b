3bb27fee942fa927b12cfb9d97a122fb9e0da1df
sjachvaecaesin <booglyboo544@gmail.com>
2023-11-07 17:20:40 -0500

    updated

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Elias Nikolai Schmutz Streeter";
+let currentYear = 2023;
+let profilePicture = "images/header picture.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+let favFoods = ["Cola Cao", "Tuna Pizza", "Hamburgers"];
+foodElement.innerHTML = favFoods;
+let newFood = "Meatloaf";
+favFoods.push(newFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
